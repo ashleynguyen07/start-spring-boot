dc7e4f0c5b283e5f27b3355518992b0b54fcc832
ashleynguyen07 <nguyentramy.se@gmail.com>
2024-01-05 11:19:12 +0700

    added 02-spring-boot-spring-core --- 01-constructor-injection

---
+++ 02-spring-boot-spring-core/01-contructor-injection/src/main/java/com/ashleynguyen/springcore/Coach.java
@@ -0,0 +1,6 @@
+package com.ashleynguyen.springcore;
+
+public interface Coach {
+    String getDailyWorkout();
+
+}
---
+++ 02-spring-boot-spring-core/01-contructor-injection/src/main/java/com/ashleynguyen/springcore/DemoController.java
@@ -0,0 +1,18 @@
+package com.ashleynguyen.springcore;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    // create a private field for the dependency
+    @Autowired
+    private Coach myCoach;
+
+    @GetMapping("/getDailyWorkout")
+    public String getDailyWorkout() {
+        return myCoach.getDailyWorkout();
+    }
+
+}
